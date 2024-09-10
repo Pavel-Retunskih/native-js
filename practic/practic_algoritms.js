3625cad4e079613975d44fee89ce4dfbdbd19f28
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-09-10 23:53:30 +0300

    fix

--- practic/practic_algoritms.js
+++ practic/practic_algoritms.js
@@ -18,6 +18,7 @@ function insertionSort(arr) {
   for (let i = 1; i < n; i++) {
     let key = arr[i];
     let j = i - 1;
+
     while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
       j = j - 1;
@@ -102,5 +103,5 @@ function bubbleSort(arr) {
 
 // Пример использования
 let arr5 = [64, 34, 25, 12, 22, 11, 90];
-bubbleSort(arr);
+bubbleSort(arr5);
 console.log("Отсортированный пузырьком массив:", arr5);
